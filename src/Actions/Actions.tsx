e9f5ae7b780d4acadf8d54ef15c623d2888d7ae8
Elizabeth Murray <liz@sterahi.com>
2020-07-24 15:55:36 -0400

    reformat buttons slightly

--- src/Actions/Actions.tsx
+++ src/Actions/Actions.tsx
@@ -14,15 +14,15 @@ export default class Actions extends React.Component {
             <div className = "Actions">
                 <div className = "button" onClick = {this.props["dviceStore"].cleanPoop}>
                     <Animation images = {['needs/poop_1', 'needs/poop_2', 'needs/poop_3', "needs/poop_2"]} speed = {500} /> 
-                    <span>Clean</span>
+                    Clean
                 </div>
                 <div className = "button" onClick = {this.props["dviceStore"].feedDigi}>
                     <Animation images = {['needs/Meat_full', 'needs/meat_3', 'needs/meat_2', 'needs/meat_1']} speed = {800} /> 
-                    <span>Feed</span>
+                    Feed
                 </div>
                 <div className = "button" onClick = {this.props["dviceStore"].medicine}>
                     <Animation images = {['needs/needle_1', 'needs/needle_2']} speed = {500} /> 
-                    <span>Medicine</span>
+                    Medicine
                 </div>
             </div>
         )
